37998f7070abd9cfdc5b51ab6519c6b70b968b4c
Joe Moudrik <j.moudrik@gmail.com>
2019-08-08 17:14:18 +0200

    __init__: add __all__

--- jmpy/__init__.py
+++ jmpy/__init__.py
@@ -1,1 +1,27 @@
 from .utils import *
+
+__all__ = [
+         'argmax',
+ 'argmax_index',
+ 'argmin',
+ 'argmin_index',
+ 'bucket_by_key',
+ 'cache_into',
+ 'collapse_whitespace',
+ 'consuming_length',
+ 'filter_both',
+ 'filter_null',
+ 'first_true_pred',
+ 'flatten',
+ 'group_consequent',
+ 'identity',
+ 'k_grams',
+ 'mod_stdout',
+ 'nonempty_strip',
+ 'prefix_stdout',
+ 'simple_tokenize',
+ 'stderr',
+ 'timer',
+ 'uniq',
+ 'utils']
+
