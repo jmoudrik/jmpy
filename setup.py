b363b1690499719a9dd7b07c47697e30b75b2e18
Joe Moudrik <j.moudrik@gmail.com>
2019-08-08 14:35:49 +0200

    setup.py: simplistic initial impl

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmpy',
+    version='1.0.0',
+    url='https://github.com/jmoudrik/jmpy.git',
+    author='Josef Moudřík',
+    author_email='j.moudrik@gmail.com',
+    description="jm's Python utils",
+    packages=['jmpy'],
+    install_requires=['numpy >= 1.11.1'],
+)
