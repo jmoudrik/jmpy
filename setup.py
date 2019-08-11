f18c6f1e8816070cf1ded4a95da62d043e52955a
Joe Moudrik <j.moudrik@gmail.com>
2019-08-11 17:16:13 +0200

    setup: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmpy',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/jmoudrik/jmpy.git',
     author='Josef Moudřík',
     author_email='j.moudrik@gmail.com',
