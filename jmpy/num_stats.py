c9add22c2037bd8ddaebd93dd69381a5af23d779
Joe Moudrik <j.moudrik@gmail.com>
2019-08-15 15:14:50 +0200

    fix import

--- jmpy/num_stats.py
+++ jmpy/num_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import utils
+import jmpy.utils
 import sys
 
 if __name__ == "__main__":
@@ -13,6 +13,6 @@ if __name__ == "__main__":
             print("skipping line '%s'"%(line[:5]), file=sys.stderr)
 
 
-    stats = utils.num_stats(nums)
+    stats = jmpy.utils.num_stats(nums)
     for k,(v, fmt) in stats.items():
         print("\t%s\t%s"%(k, fmt%v))
