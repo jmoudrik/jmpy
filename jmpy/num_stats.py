07affaa4e168d11b64237aa78da75e752776c172
Joe Moudrik <j.moudrik@gmail.com>
2020-10-22 16:00:11 +0200

    num_stats: add plenty of cmdline arguments

--- jmpy/num_stats.py
+++ jmpy/num_stats.py
@@ -1,23 +1,69 @@
 #!/usr/bin/env python3
 
-import jmpy.utils
 import sys
+import argparse
+from itertools import chain
 
-if __name__ == "__main__":
-    numbers = []
-    for line in sys.stdin:
+import jmpy.utils
+
+
+def iter_lines(filename):
+    with open(filename, 'r') as fin:
+        return fin.readlines()
+
+
+def iter_input(filename, silent):
+    if filename == '-':
+        filename = "STDIN"
+        lines = sys.stdin
+    else:
+        lines = iter_lines(filename)
+    for num, line in enumerate(lines):
         line = line.strip()
         if not line:
             continue
         try:
-            numbers.append(float(line))
-        except:
-            print("skipping line '%s'"%(line[:5]), file=sys.stderr)
+            yield float(line)
+        except ValueError:
+            if not silent:
+                print("file '%s': skipping line %d: '%s%s'" % (
+                    filename, num, line[:20], '...' if len(line) >= 23 else line[20:]), file=sys.stderr)
 
-    def has_arg(l):
-        return len(sys.argv) >= 2 and any(a in l for a in sys.argv[1:])
 
-    plot = has_arg(['-h', '--hist'])
+def iter_inputs(files, silent):
+    return chain.from_iterable(iter_input(file, silent) for file in files)
 
-    jmpy.utils.full_stats(numbers, plot, bins='doane')
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description="""
+    Print statistics of numbers. Numbers are read from STDIN.
+    This program stores all numbers in memory before the computation
+    and is thus NOT suitable for large number of inputs.
+    """)
+
+    parser.add_argument("-s", "--sum-histogram", help="Plot histogram of sums of values in bins.", action="store_true")
+    parser.add_argument("-c", "--count-histogram", help="Plot normal histogram of values.", action="store_true")
+    parser.add_argument("--bins", type=str,
+                        help="Specify numpy histogram bin algorithm to use (e.g. sturges, doane, fd).",
+                        default='doane')
+    parser.add_argument("-w", "--histogram-width", type=int, help="""
+    Specify max width of the histogram plot.
+    Just the '*', excluding labels on the left.
+    """, default=50)
+    parser.add_argument("--silent", help="""
+    If set, program silently skips any lines that are not a number.
+    """, action='store_true')
+    parser.add_argument('FILENAME', nargs='*', help="""
+    Files to read the numbers from. Assumes one-line per number ('\\n' separated).
+    Not listing any files, or including FILENAME of '-' causes the program to read inputs from STDIN.
+    """, default=[])
+    args = parser.parse_args()
+
+    files = args.FILENAME
+    if not files:
+        files = ['-']
+
+    numbers = list(iter_inputs(files, args.silent))
+    jmpy.utils.full_stats(numbers, sum_hist=args.sum_histogram, count_hist=args.count_histogram, bins=args.bins,
+                          max_bin_width=args.histogram_width)
